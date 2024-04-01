2390b4d2c7fd778ec9e35b0b5891f16378a003fe
stevenwooden <161550247+stevenwooden@users.noreply.github.com>
2024-03-31 20:10:25 -0400

    lab 2 complete

---
+++ lab2.js
@@ -0,0 +1,20 @@
+function sum(a,b){
+    return a + b;
+}
+function sub(a,b){
+    return a - b
+}
+function multiply(a,b){
+    return a * b
+}
+function divide (a,b){
+    return a / b
+}
+console.log(sum(10,20))
+console.log(sub(50,23))
+console.log(multiply(13,17))
+console.log(divide(202,4));
+function greeting(name){
+    return "Hello " + " " + name;
+}
+console.log(greeting("Steven"))
